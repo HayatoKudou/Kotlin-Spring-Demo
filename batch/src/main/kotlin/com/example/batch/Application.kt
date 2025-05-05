f3f40124f869488a915f095d2c58c324feb33d70
HayatoKudo <kudoh115@gmail.com>
2025-05-05 23:24:50 +0900

    feat: 簡単なCRUD処理を実行するバッチの実装

--- batch/src/main/kotlin/com/example/batch/Application.kt
+++ batch/src/main/kotlin/com/example/batch/Application.kt
@@ -2,16 +2,18 @@ package com.example.batch
 
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.ExitCodeGenerator
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.ExitCodeGenerator
 import org.springframework.jdbc.core.JdbcTemplate
+import java.util.function.Consumer
+import java.util.stream.Collectors
+
 
 @SpringBootApplication
 class Application(
     private val jdbcTemplate: JdbcTemplate
 ) : CommandLineRunner, ExitCodeGenerator {
-    @Throws(Exception::class)
     override fun run(vararg strings: String?) {
         val logger = LoggerFactory.getLogger(this::class.java)
         logger.info("バッチ処理を開始します")
@@ -21,6 +23,30 @@ class Application(
             "CREATE TABLE customers(" +
                     "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))"
         )
+
+        val splitUpNames = listOf("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long")
+            .map { it.split(" ") }
+
+        splitUpNames.forEach { (first, last) ->
+            logger.info("Inserting customer record for $first $last")
+        }
+
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)",
+            splitUpNames.map { it.toTypedArray() })
+
+        jdbcTemplate.query(
+            "SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+            { rs, _ ->
+                Customer(
+                    rs.getLong("id"),
+                    rs.getString("first_name"),
+                    rs.getString("last_name")
+                )
+            },
+            "Josh"
+        ).forEach { customer ->
+            logger.info(customer.toString())
+        }
 
         logger.info("バッチ処理が完了しました")
     }
@@ -32,4 +58,13 @@ class Application(
 
 fun main(args: Array<String>) {
     SpringApplication.exit(SpringApplication.run(Application::class.java, *args))
+}
+
+class Customer(private val id: Long, private val firstName: String?, private val lastName: String?) {
+    override fun toString(): String {
+        return String.format(
+            "Customer[id=%d, firstName='%s', lastName='%s']",
+            id, firstName, lastName
+        )
+    }
 }
