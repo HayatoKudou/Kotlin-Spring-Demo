1fce0027e3319caa9740d1450defc36301b37b7c
HayatoKudo <kudoh115@gmail.com>
2025-05-17 23:30:24 +0900

    chore: CORS設定

--- api/src/main/kotlin/com/example/api/controller/TodoController.kt
+++ api/src/main/kotlin/com/example/api/controller/TodoController.kt
@@ -2,20 +2,16 @@ package com.example.api.controller
 
 import com.example.api.dao.todo.TodoDTO
 import com.example.api.dao.todo.TodoDAO
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.*
 
 const val URL_BASE = "/api/v1"
 
 @RestController
 @RequestMapping(path = [URL_BASE])
 class TodoController(private val todoDao: TodoDAO) {
-
     @RequestMapping(
         path = ["/todos"],
         method = [RequestMethod.GET],
